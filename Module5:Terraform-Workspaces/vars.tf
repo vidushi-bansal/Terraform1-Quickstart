7933a97ed89d387fda1bdc57136c567ea8f41ee7
vidushi-bansal <vidushibansal13@gmail.com>
2021-05-25 12:20:33 +0530

    Added Module5

--- Module5:Terraform-Workspaces/vars.tf
+++ Module5:Terraform-Workspaces/vars.tf
@@ -2,6 +2,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "private_key_path" {}
 variable "key_name" {}
+variable "owner" {}
 variable "region" {
   default = "us-east-1"
 }
